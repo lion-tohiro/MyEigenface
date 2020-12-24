ceff1211bd122e3d2c7a62ba08966600ffe97484
lion-tohiro <3170094587@qq.com>
2020-12-24 21:59:19 +0800

    update eigenface.py 2020/12/24 21:59

--- eigenface.py
+++ eigenface.py
@@ -6,16 +6,36 @@ import PIL
 
 train_sub_count = 40
 train_img_count = 5
+total_face = 200
+row = 112
+col = 92
 
-def read_image(src_path):
-    img_list = []
+def eigenfaces(src_path):
+    img_list = np.empty((row*col, total_face))
+    count = 0
 
-    for i in range(1, train_sub_count + 1):
-        for j in range(1, 6):
+    #read all the faces and flatten them
+    for i in range(1, train_sub_count):
+        for j in range(1, train_img_count):
             img_path = src_path + "/s" + str(i) + "/" + str(j) + ".pgm"
-            img = cv2.imread(img_path)
-            img_list.append(img)
-    
-    return img_list
+            #print(img_path)
+            img = cv2.imread(img_path, 0)
+            #cv2.imshow("1",img)
+            img_col = np.array(img).flatten()
 
+            img_list[:, count] = img_col[:]
+            count += 1
+    
+    #compute the average of the faces
+    img_mean = np.sum(img_list, axis=1) / total_face
+    # average = img_mean.reshape(row, col).astype(np.uint8)
+    # cv2.imshow("ave", average)
+    # cv2.waitKey(0)
+
+    #compute the delta
+    for i in range(0, total_face):
+        img_list[:, i] -= img_mean[:]
+    
+
+eigenfaces("D:\\k\\Myeigen\\MyEigenface\\att_faces")
     
